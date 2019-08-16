c881fc54713a42289ab803900e65668e6f413c68
Javin Hung <jh@enfuse.io>
2019-08-16 15:51:24 -0700

    changed source to a file. added scripts to auto generate file and put onto server
    Signed-off-by: Tom Butler <tb@enfuse.io>

--- tools/deploy-stream.sh
+++ tools/deploy-stream.sh
@@ -5,7 +5,7 @@ set -eu
 
 curl -H "Accept: application/json" -X POST "http://localhost:9393/streams/definitions" -i \
     -d "name=geode-demo-pipeline" \
-    -d "definition=http --server.port=20000|transform|log"
+    -d "definition=file --server.port=20000 --mode=lines --file.directory=/tmp/foo/ |transform|log"
 
 curl -H "Content-type: application/json" -X POST "http://localhost:9393/streams/deployments/geode-demo-pipeline" -i#
  \
