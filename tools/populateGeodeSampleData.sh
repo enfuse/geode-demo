c6e2956964299eec8a79fafc1a21068a0931d0ec
Javin Hung <jh@enfuse.io>
2019-08-27 13:33:45 -0700

    streamlined data import a bit more
    Signed-off-by: Tom Butler <tb@enfuse.io>

--- tools/populateGeodeSampleData.sh
+++ tools/populateGeodeSampleData.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-docker cp ./data/100k.gfd server:/tmp/100k.gfd
-docker cp ./tools/uploadGeodeData.gfsh locator:/tmp/
-docker exec -ti locator sh -c 'gfsh run --file=/tmp/uploadGeodeData.gfsh'
+docker exec -ti locator sh -c 'gfsh run --file=/docker/geode/importGeodeData.gfsh'
