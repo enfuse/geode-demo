fc3698b529ee536b5e544928ca24555649e1e4ad
Javin Hung <jh@enfuse.io>
2019-08-27 14:58:36 -0700

    added rebalance command to account for variable geode nodes and added auto load to server group script
    Signed-off-by: Tom Butler <tb@enfuse.io>

--- docker/geode/startServerGroup.sh
+++ docker/geode/startServerGroup.sh
@@ -6,6 +6,8 @@ gfsh start server --name=server1 --group=group1 --server-port=40404 --start-rest
 gfsh start server --name=server2 --group=group1 --server-port=40405 --start-rest-api=true --cache-xml-file=/docker/geode/cache.xml  --locators=locator.localhost[10334] --dir=/data/server2/ "$@"
 gfsh start server --name=server3 --group=group1 --server-port=40406 --start-rest-api=true --cache-xml-file=/docker/geode/cache.xml  --locators=locator.localhost[10334] --dir=/data/server3/ "$@"
 
+gfsh run --file=/docker/geode/importGeodeData.gfsh
+
 while true; do
     sleep 10
   done
